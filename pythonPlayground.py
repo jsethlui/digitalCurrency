8b93834a7b182575706ae054b530916b331b99ad
Jeremy Louie <43184783+jsethlui@users.noreply.github.com>
2021-05-20 19:31:48 -0700

    Added database read
    To secure default API key

--- pythonPlayground.py
+++ pythonPlayground.py
@@ -3,18 +3,35 @@ import curses
 import os
 import sys
 import requests
-from time import sleep
+import time as t
+import pyrebase
 from datetime import datetime
 
+# global data
 idBase = ""
+apiKey = ""
+userHasKey = False
 rateMapping = {"DOGE": "Dogecoin", 
                 "ETH": "Ethereum", 
                 "BTC": "Bitcoin", 
-                "LTC": "Litecoin"}
+                "LTC": "Litecoin"}              
+
+def checkModules():
+    try:
+        print("asdf")
+        # import curses
+        # import os
+        # import sys
+        # import requests
+        # import time as t
+        # import subprocess
+        # from datetime import datetime
+        # from firebase_admin import db 
+    except ImportError:
+        pass
 
 def getCryptoCurrencyPrice(cryptoBase):
     url = "https://rest.coinapi.io/v1/exchangerate/" + str(cryptoBase) + "/USD"
-    apiKey = "F630E2F0-8BBC-4DDF-AE7B-41E2F54B97D2"
     header = {"X-CoinAPI-Key": apiKey}
     response = requests.get(url, headers = header)
     responseJson = response.json()
@@ -23,6 +40,19 @@ def getCryptoCurrencyPrice(cryptoBase):
         sys.stderr.write(Color.FAIL + "Error: " + Color.END + "max requests exceeded (status code = " + str(statusCode) + ")\n")
         sys.exit(1)
     return responseJson
+
+def setApiKey(name = "Default"):
+    global apiKey
+    config = {
+        "apiKey": "",
+        "authDomain": "",
+        "databaseURL": "https://getcrypto-2ab92-default-rtdb.firebaseio.com/",
+        "storageBucket": ""
+    }  
+    firebase = pyrebase.initialize_app(config)
+    database = firebase.database()
+    data = database.child("Default").get()
+    apiKey = data.val()["Key"]
 
 def getTime():
     currentDate = datetime.now().strftime("%d/%m/%Y")
@@ -46,15 +76,18 @@ def main(argv):
         sys.stderr.write("\033[1m" + "Error: " + "\033[0m" + "invalid mapping " + str(idBase) +  "\n")
         sys.exit(1)
 
+    setApiKey()     # set to default key
     curses.wrapper(cursesMain)
     sys.exit(0)
 
 def cursesMain(window):
     global idBase
-
-    window.nodelay(True)   # enable refreshing  
-    maxX = window.getmaxyx()[0]
-    maxY = window.getmaxyx()[1]
+    global userHasKey
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    window.bkgd(1, curses.color_pair(1))
+    window.nodelay(True)        # enable refreshing  
+    maxX = window.getmaxyx()[0] # get max y
+    maxY = window.getmaxyx()[1] # get max x
     currentRow = 5
 
     # instantiating bar
@@ -64,27 +97,29 @@ def cursesMain(window):
 
     refreshRate = 840   # fourteen minutes
     timeElapsed = refreshRate
-
-    # sync up with real time
-    sleepTime = 1 - datetime.now().second % 1
-    sleep(sleepTime * 60)
-
+    t.sleep(1 - (t.time() % 1))
     while (True):
         window.timeout(1000)        # block every second
 
-        # crypto base (top left)
-        window.addstr(1, 1, "Currency: ", curses.A_BOLD)
-        window.addstr(1, 11, rateMapping.get(idBase) + " (" + idBase + ")")
+        # user info
+        if ((userHasKey == False) and (apiKey != "")):
+            window.addstr(0, maxY - 12, "Default Key", curses.A_BOLD)
+        elif ((userHasKey == True) and (apiKey == "")):
+            window.addstr(0, maxY - 15, "Registered Key", curses.A_BOLD)
 
-        # refresh rate (left of crypto base)
-        window.addstr(1, 31, "Refresh Rate: ", curses.A_BOLD)
-        window.addstr(1, 45, str(int(refreshRate / 60)) + " minutes")
-        window.addstr(2, 45, "(" + str(int(refreshRate)) + " seconds)")
+        # crypto base 
+        window.addstr(2, 1, "Currency: ", curses.A_BOLD)
+        window.addstr(2, 11, rateMapping.get(idBase) + " (" + idBase + ")")
+
+        # refresh rate
+        window.addstr(2, 31, "Refresh Rate: ", curses.A_BOLD)
+        window.addstr(2, 45, str(int(refreshRate)) + " seconds")
+        window.addstr(2, 45, "(" + str(int(refreshRate / 60)) + " minutes)")
 
         # time elapsed
-        window.addstr(1, 61, "Time Elapsed: ", curses.A_BOLD)
-        window.addstr(1, 75, str(timeElapsed) + " / " + str(refreshRate) + " seconds")
-        window.addstr(2, 75, str(round(timeElapsed / refreshRate, 4) * 100) + " %")
+        window.addstr(2, 61, "Time Elapsed: ", curses.A_BOLD)
+        window.addstr(2, 75, str(timeElapsed) + " / " + str(refreshRate) + " seconds")
+        window.addstr(3, 75, str(round(timeElapsed / refreshRate, 4) * 100) + " %")
         timeElapsed -= 1
         if (timeElapsed < 0):
             timeElapsed = refreshRate
@@ -103,12 +138,18 @@ def cursesMain(window):
         if (currentRow == 23):   # 20 + 3 = 23
             currentRow = 5
 
+        # settings 
+        window.addstr(maxX - 1, maxY - 11, "(SETTINGS)", curses.A_BOLD | curses.A_REVERSE | curses.A_STANDOUT)
 
-        # quit (bottom left)
+        # quit
         window.addstr(maxX - 1, 0, "(QUIT)", curses.A_BOLD | curses.A_REVERSE | curses.A_STANDOUT)
         window.refresh()
         userChar = window.getch()
+
         if (userChar == ord("q") or (userChar == ord("Q"))):
+            curses.endwin()
+            break
+        elif (userChar == ord("s") or (userChar == ord("S"))):
             curses.endwin()
             break
         window.clear()
